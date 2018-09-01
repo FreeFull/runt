30e24872bf994ea1da46b18b8a7db0729b2dab20
Filip Szczepański <jazz2rulez@gmail.com>
2018-09-01 15:47:48 +0200

    Switch from reqwest to hyper

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+#[macro_use]
 extern crate failure;
+extern crate bytes;
+extern crate futures;
 extern crate html5ever;
-extern crate reqwest;
+extern crate hyper;
+extern crate hyper_tls;
+extern crate tokio;
 
 use std::default::Default;
 use std::env::args;
@@ -9,18 +14,51 @@ use html5ever::parse_document;
 use html5ever::rcdom::{Node, NodeData, RcDom};
 use html5ever::tendril::TendrilSink;
 
+use futures::stream::Stream;
+use futures::Future;
+
+use bytes::Buf;
+
 fn main() {
     run().unwrap();
 }
 
 fn run() -> Result<(), failure::Error> {
     let url = args().nth(1);
-    let url = url.as_ref().map(|url| &url[..]).unwrap_or("https://www.rust-lang.org");
-    let body = reqwest::get(url)?.text()?;
-    let dom = parse_document(RcDom::default(), Default::default())
-        .from_utf8()
-        .read_from(&mut body.as_bytes())?;
-    walk(&dom.document, 0);
+    let url = url
+        .as_ref()
+        .map(|url| &url[..])
+        .unwrap_or("https://www.rust-lang.org")
+        .parse()?;
+    tokio::run(futures::lazy(|| {
+        let https = hyper_tls::HttpsConnector::new(4).unwrap();
+        let client = hyper::Client::builder().build::<_, hyper::Body>(https);
+        client
+            .get(url)
+            .from_err()
+            .and_then(|response| {
+                if response.status() != hyper::StatusCode::OK {
+                    bail!("HTTP status code: {}", response.status())
+                } else {
+                    Ok(response.into_body().concat2().from_err())
+                }
+            }).and_then(|body| body)
+            .and_then(|body| {
+                let dom = parse_document(RcDom::default(), Default::default())
+                    .from_utf8()
+                    .read_from(&mut body.reader())?;
+                walk(&dom.document, 0);
+                Ok(())
+            }).then(|result| {
+                match result {
+                    Ok(_) => {}
+                    Err(err) => {
+                        println!("Error: {}", err);
+                    }
+                }
+                Ok(())
+            })
+    }));
     Ok(())
 }
 
